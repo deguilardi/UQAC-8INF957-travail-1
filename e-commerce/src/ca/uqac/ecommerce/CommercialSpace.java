8611155ad9fb04ca11972d1e4381e249d6b87f7d
deguilardi <demetrio.guilardi@gmail.com>
2020-09-24 19:10:15 -0400

    Commercial Space data structures changed to Map

--- e-commerce/src/ca/uqac/ecommerce/CommercialSpace.java
+++ e-commerce/src/ca/uqac/ecommerce/CommercialSpace.java
@@ -3,6 +3,7 @@ package ca.uqac.ecommerce;
 import ca.uqac.ecommerce.party.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * - There are several planets (at least 5 in our example);
@@ -21,66 +22,67 @@ import java.util.ArrayList;
  */
 public class CommercialSpace {
 
-    private final ArrayList<Spaceship> spaceships = new ArrayList<>();
-    private final ArrayList<Planet> planets = new ArrayList<>();
+    private final HashMap<String, Product> products = new HashMap<>();
+    private final HashMap<String, Spaceship> spaceships = new HashMap<>();
+    private final HashMap<String, Planet> planets = new HashMap<>();
 
     public void initialize(){
-        // Set some products
-        Product gravel = new Product("gravel", 10, 1.0f, Product.Menacing.REGULAR);
-        Product tires = new Product("tires", 3, 12.75f, Product.Menacing.REGULAR);
-        Product acid = new Product("acid", 4, 7.5f, Product.Menacing.DANGEROUS);
+        // Init products
+        products.put("gravel", new Product("gravel", 10, 1.0f, Product.Menacing.REGULAR));
+        products.put("tires", new Product("tires", 3, 12.75f, Product.Menacing.REGULAR));
+        products.put("acid", new Product("acid", 4, 7.5f, Product.Menacing.DANGEROUS));
 
         // Init spaceships
         // Cruise
         ArrayList<Container> cruiseShipContainers = new ArrayList<>();
-        cruiseShipContainers.add(new Container(gravel, 10, 0));
-        cruiseShipContainers.add(new Container(acid, 10, 0));
-        spaceships.add(new CruiseShip("cruise", cruiseShipContainers));
+        cruiseShipContainers.add(new Container(products.get("gravel"), 10, 0));
+        cruiseShipContainers.add(new Container(products.get("acid"), 10, 0));
+        spaceships.put("cruise", new CruiseShip("cruise", cruiseShipContainers));
         // Millenial
         ArrayList<Container> millenialShipContainers = new ArrayList<>();
-        millenialShipContainers.add(new Container(gravel, 10, 0));
-        millenialShipContainers.add(new Container(tires, 10, 0));
-        spaceships.add(new MillenialShip("millenial", millenialShipContainers));
+        millenialShipContainers.add(new Container(products.get("gravel"), 10, 0));
+        millenialShipContainers.add(new Container(products.get("tires"), 10, 0));
+        spaceships.put("millenial", new MillenialShip("millenial", millenialShipContainers));
         // Ultra
         ArrayList<Container> ultraShipContainers = new ArrayList<>();
-        ultraShipContainers.add(new Container(tires, 10, 0));
-        ultraShipContainers.add(new Container(acid, 10, 0));
-        spaceships.add(new UltraShip("ultra", ultraShipContainers));
+        ultraShipContainers.add(new Container(products.get("tires"), 10, 0));
+        ultraShipContainers.add(new Container(products.get("acid"), 10, 0));
+        spaceships.put("ultra", new UltraShip("ultra", ultraShipContainers));
 
         // Init planets
         // Venus
         ArrayList<Container> venusConteiners = new ArrayList<>();
-        venusConteiners.add(new Container(gravel, 20, 10));
-        venusConteiners.add(new Container(acid, 20, 10));
-        planets.add(new Planet("venus", venusConteiners, 3));
+        venusConteiners.add(new Container(products.get("gravel"), 20, 10));
+        venusConteiners.add(new Container(products.get("acid"), 20, 10));
+        planets.put("venus", new Planet("venus", venusConteiners, 3));
         // Mars
         ArrayList<Container> marsContainers = new ArrayList<>();
-        marsContainers.add(new Container(gravel, 20, 10));
-        marsContainers.add(new Container(tires, 20, 10));
-        planets.add(new Planet("mars", marsContainers, 3));
+        marsContainers.add(new Container(products.get("gravel"), 20, 10));
+        marsContainers.add(new Container(products.get("tires"), 20, 10));
+        planets.put("mars", new Planet("mars", marsContainers, 3));
         // Neptune
         ArrayList<Container> neptuneContainers = new ArrayList<>();
-        neptuneContainers.add(new Container(tires, 20, 10));
-        neptuneContainers.add(new Container(acid, 20, 10));
-        planets.add(new Planet("neptune", neptuneContainers, 3));
+        neptuneContainers.add(new Container(products.get("tires"), 20, 10));
+        neptuneContainers.add(new Container(products.get("acid"), 20, 10));
+        planets.put("neptune", new Planet("neptune", neptuneContainers, 3));
 
-
-        // Initialization reports
-        // Spaceships
-        System.out.println("[Init] spaceships");
-        reportSpaceships();
-        // Planets
-        System.out.println("[Init] planets");
-        reportPlanets();
+        // Report
+        System.out.println("[CommercialSpace] initialization finished");
+        reportAll();
     }
 
     private void reportSpaceships(){
         System.out.println("[CommercialSpace] Reporting spaceships");
-        spaceships.forEach(Party::report);
+        spaceships.forEach((index, spaceship) -> spaceship.report());
     }
 
     private void reportPlanets(){
         System.out.println("[CommercialSpace] Reporting planets");
-        planets.forEach(Party::report);
+        planets.forEach((index, planet) -> planet.report());
+    }
+
+    private void reportAll(){
+        reportSpaceships();
+        reportPlanets();
     }
 }
