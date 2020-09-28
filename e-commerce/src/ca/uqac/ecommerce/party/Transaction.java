f74f9eb305b0699030a2b0fcd3260ed35b1ad95b
deguilardi <demetrio.guilardi@gmail.com>
2020-09-28 17:14:37 -0400

    removed unnused methods

--- e-commerce/src/ca/uqac/ecommerce/party/Transaction.java
+++ e-commerce/src/ca/uqac/ecommerce/party/Transaction.java
@@ -14,38 +14,6 @@ public class Transaction {
         this.quantity = quantity;
     }
 
-    public String getBuyerName() {
-        return buyerName;
-    }
-
-    public void setBuyerName(String buyerName) {
-        this.buyerName = buyerName;
-    }
-
-    public String getSellerName() {
-        return sellerName;
-    }
-
-    public void setSellerName(String sellerName) {
-        this.sellerName = sellerName;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
     @Override
     public String toString() {
         return new StringBuilder()
